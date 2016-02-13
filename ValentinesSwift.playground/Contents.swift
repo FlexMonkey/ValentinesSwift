463fc4e32fe37e73f7c879e2031268ac7eb2c08e
Simon Gladman <sgladman@gmail.com>
2016-02-13 06:59:51 +0000

    Round bottom :)

--- ValentinesSwift.playground/Contents.swift
+++ ValentinesSwift.playground/Contents.swift
@@ -8,6 +8,7 @@ layer.strokeColor = UIColor.redColor().CGColor
 layer.fillColor = UIColor(red: 1, green: 0.33, blue: 0.33, alpha: 1).CGColor
 layer.lineWidth = 12
 layer.lineCap = kCALineCapRound
+layer.lineJoin = kCALineCapRound
 
 let view = UIView(frame: CGRect(x: 0, y: 0, width: 640, height: 640 ))
 view.layer.addSublayer(layer)
