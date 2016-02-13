8ce8e4a7449a4b83372f395ab0d7e4c6685707ad
Simon Gladman <sgladman@gmail.com>
2016-02-13 06:44:04 +0000

    Formatting

--- ValentinesSwift.playground/Contents.swift
+++ ValentinesSwift.playground/Contents.swift
@@ -15,15 +15,15 @@ view.layer.addSublayer(layer)
 XCPlaygroundPage.currentPage.liveView = view
 
 let points: [CGPoint] = 0.stride(to: M_PI * 2, by: 0.01).map
-    {
-        let x = pow(sin($0), 3)
-        var y = 13 * cos($0)
-        y -= 5 * cos(2 * $0)
-        y -= 2 * cos(3 * $0)
-        y -= cos(4 * $0)
-        y /= 16
-        
-        return CGPoint(x: 320 + (x * 300), y: 280 + (y * -300))
+{
+    let x = pow(sin($0), 3)
+    var y = 13 * cos($0)
+    y -= 5 * cos(2 * $0)
+    y -= 2 * cos(3 * $0)
+    y -= cos(4 * $0)
+    y /= 16
+    
+    return CGPoint(x: 320 + (x * 300), y: 280 + (y * -300))
 }
 
 let path = CGPathCreateMutable()
